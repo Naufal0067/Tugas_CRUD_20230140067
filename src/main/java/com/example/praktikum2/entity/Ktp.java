67143aaa2370dd2e69ac707c266aa20af0ae29fe
Naufal0067 <naufalopang1928@gmail.com>
2026-03-13 20:59:30 +0700

    menambahkan code untuk entity dengan nama tabel ktp

---
+++ src/main/java/com/example/praktikum2/entity/Ktp.java
@@ -0,0 +1,17 @@
+package com.example.praktikum2.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "ktp")
+@Data
+public class Ktp {
+
+  
+
+
+
+}
